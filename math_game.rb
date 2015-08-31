5e34ebcddb20b44d01635fbabbe61202efe40bab
Gabriela Cevallos <gabriela_cevallos@hotmail.com>
2015-08-31 17:40:12 -0400

    Working game copy

--- math_game.rb
+++ math_game.rb
@@ -1,7 +1,54 @@
-def random_number
- 
- puts rand(1..10)
+@health_1 = 3
+
+@health_2 = 3
+
+@alive = true 
+
+def game
+
+	while @alive
+
+		@first_number = rand(1..20)
+		@second_number = rand(1..20)
+		@answer = @first_number + @second_number
+
+		puts "Player 1: What does #{@first_number} plus #{@second_number} equal?"
+			
+		hello = gets.chomp
+
+		if hello.to_i == @answer
+			puts "You got it right! You have #{@health_1} lives left."
+
+		else 
+			@health_1 -= 1
+
+			if @health_1 == 0
+				@alive = false
+			else
+				puts "You're answer is wrong. You have #{@health_1} lives left."
+			end
+		end
+
+		first_number = rand(1..20)
+		@second_number = rand(1..20)
+		@answer = @first_number + @second_number
+
+		puts "Player 2: What does #{@first_number} plus #{@second_number} equal?"
+			hi = gets.chomp
+			if hi.to_i == @answer
+			puts "You got it right! You have #{@health_2} lives left."
+
+		else 
+			@health_2 -= 1
+
+			if @health_2 == 0
+				@alive = false
+			else
+				puts "You're answer is wrong. You have #{@health_2} lives left."
+			end
+		end	
 
 end
+end
 
-random_number
+game
